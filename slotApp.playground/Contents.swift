35773dcf962665b7b36afd7af7858b80105540fe
HiroHosokawa <soccer8.0802@gmail.com>
2022-11-26 17:51:43 +0900

    slotAppを編集

--- slotApp.playground/Contents.swift
+++ slotApp.playground/Contents.swift
@@ -8,44 +8,60 @@
 
 import UIKit
 
-    var inputMedal = 10
+class SlotMachien {
 
+    var inputMedal = 10
+    var firstItem = Int.random(in: 0...5)
+    var secondItem = Int.random(in: 0...5)
+    var thirdItem = Int.random(in: 0...5)
+    
+    func check() -> Bool {
+        let startSlot = inputMedal >= 3
+        if startSlot {
+            inputMedal -= 3
+        }
+        return startSlot
+    }
+    
     func slotItem(firstItem: Int, secondItem: Int, thirdItem: Int) -> String {
             let result = String(firstItem) + String(secondItem) + String(thirdItem)
             return result
     }
-    
-    let firstItem = Int.random(in: 0...5)
-    let secondItem = Int.random(in: 0...5)
-    let thirdItem = Int.random(in: 0...5)
-    let sumItem = slotItem(firstItem: Int.random(in: 1...3), secondItem: Int.random(in: 1...3), thirdItem: Int.random(in: 1...3))
-    print(sumItem)
+}
 
-    func startSlot() {
-        if sumItem == "111" && inputMedal >= 3  {
-            inputMedal -= 3
-            inputMedal += 100
-            print("111が当たりました。100枚の払い出しで残メダルは\(inputMedal)枚です。")
-        } else if sumItem == "222" {
-            inputMedal -= 3
-            inputMedal += 200
-            print("222が当たりました。200枚の払い出しで残メダルは\(inputMedal)枚です。")
-        } else if sumItem == "333" {
-            inputMedal -= 3
-            inputMedal += 300
-            print("333が当たりました。300枚の払い出しで残メダルは\(inputMedal)枚です。")
-        } else if inputMedal <= 3 {
-            print("メダルが足りません。残メダルは\(inputMedal)枚です。")
-        } else if inputMedal >= 3 {
-            inputMedal -= 3
-            print("ハズレました。残メダルは\(inputMedal)枚です。")
-        }
+let slotMachien = SlotMachien()
+let checkMedal = slotMachien.check()
+slotMachien.inputMedal = 3
+
+let sumItem = slotMachien.slotItem(firstItem: Int.random(in: 1...3), secondItem: Int.random(in: 1...3), thirdItem: Int.random(in: 1...3))
+
+print("現在のメダルは\(slotMachien.inputMedal)枚です。")
+
+if checkMedal == true {
+    print("スロットを回します！")
+    }else {
+    print("メダルが足りません。追加投入して下さい")
+}
+
+func slotResult() {
+    if checkMedal != true {
+        print("メダルが足りません。残メダルは\(slotMachien.inputMedal)枚です。")
+    } else if sumItem == "111" && slotMachien.inputMedal >= 3  {
+        slotMachien.inputMedal += 100
+        print("111が当たりました。100枚の払い出しで残メダルは\(slotMachien.inputMedal)枚です。")
+    } else if sumItem == "222" {
+        slotMachien.inputMedal += 200
+        print("222が当たりました。200枚の払い出しで残メダルは\(slotMachien.inputMedal)枚です。")
+    } else if sumItem == "333" {
+        slotMachien.inputMedal += 300
+        print("333が当たりました。300枚の払い出しで残メダルは\(slotMachien.inputMedal)枚です。")
+        
+    } else  {
+        print("ハズレました。残メダルは\(slotMachien.inputMedal)枚です。")
     }
+}
 
-    print(startSlot())
-
-    
-
-
-
+print(sumItem)
+slotMachien.inputMedal -= 3
+print(slotResult())
 
